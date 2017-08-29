a856e0e05aad62d742b33179849851829496a829
Martin Kolárik <martin@kolarik.sk>
2017-08-29 23:13:52 +0200

    Support for output formats other than ES6

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { extname } from 'path';
 import Ractive from 'ractive';
 import rcu from 'rcu';
-import { es6 } from 'rcu-builders';
+import * as builders from 'rcu-builders';
 import { createFilter } from 'rollup-pluginutils';
 
 rcu.init( Ractive );
 
 export default function dsv ( options = {} ) {
 	const filter = createFilter( options.include, options.exclude );
-
 	const extensions = options.extensions || [ '.html' ];
+	const format = options.format || 'es6';
 
 	return {
 		name: 'ractive',
@@ -20,7 +20,7 @@ export default function dsv ( options = {} ) {
 			if ( !~extensions.indexOf( extname( id ) ) ) return null;
 
 			const definition = rcu.parse( code, options.parseOptions );
-			const module = es6( definition, {
+			const module = builders[ format ]( definition, {
 				preserveExtensions: true,
 				sourceMap: true
 			});
